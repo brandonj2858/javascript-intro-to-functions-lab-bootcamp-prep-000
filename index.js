8c2f542ebec5764c70825c0080d9ef1db5785773
Brandon James <brandonj2858@gmail.com>
2019-03-28 20:04:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-function sayHiToGrandma(string) if(string=toLowerCase)
-{console.log("I can't
+function sayHiToGrandma(string)
+if(string===toLowerCase
+{console.log("I can't hear you!")
 }
