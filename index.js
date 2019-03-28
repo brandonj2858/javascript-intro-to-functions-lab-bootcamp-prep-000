da0eba21c0379cd628f0a11b79f5c2aa12214e12
Brandon James <brandonj2858@gmail.com>
2019-03-28 20:29:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-function sayHitoGrandma(string) 
+function sayHitoGrandma(string) if(string===string.toLowerCase)
 }
