8948957fa1701ac4bbb25cbb5ebc0754867146b6
Brandon James <brandonj2858@gmail.com>
2019-03-28 21:46:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ if(string===string.toLowerCase())
 {return "I can\'t hear you!"}
 else if(string===string.toUpperCase())
 {return "YES INDEED!"}
-iftring==="I love u,Grandma")
+if(string==="I love u,Grandma")
 {return "I love you, too."}
 }
